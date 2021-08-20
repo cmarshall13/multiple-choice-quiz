e7e23719937e356567c8c38f62afd0d592949d7a
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-08-20 15:44:33 -0400

    adding functions for high score page, adding event listeners for high score page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//Set array for multiple choice questions 1-10
+
 var questionObjectArray = [
     {
         "question": "Inside which HTML element do we put the JavaScript?",
@@ -83,6 +85,8 @@ var questionObjectArray = [
 
 ];
 
+//set global varibles for elements
+
 const startButton = document.getElementById('start-btn');
 const answerA = document.getElementById('answer-a');
 const answerB = document.getElementById('answer-b');
@@ -98,9 +102,106 @@ const showEndButtons = document.querySelector('.high-score-buttons');
 const returnButton = document.querySelector('#return-button');
 const clearHighScoresButton = document.querySelector('#clear-button');
 
+//set variables for end of game
 let viewHighScores = document.querySelector('#view-score-link');
 let formElement = document.querySelector('#submit-form');
 let userInitials = document.querySelector('#initials');
 let endGameText = document.querySelector('.end-game');
 let displayedHighScore = document.querySelector('.end-sub-title');
 let leaderBoard = document.querySelector('.high-score-leaderboard');
+
+//set variables for correct/incorrect answers
+let correctAnswers = 0;
+let answerTextElement = document.querySelector('.answer-text');
+let correctAnswer = document.querySelector('.correct-text');
+let incorrectAnswer = document.querySelector('.incorrect-text');
+
+//create countdown timer
+let counter = 75;
+let timerHTMLText = document.getElementById('countdown');
+var beginCountdown;
+
+//create high scores array
+let highScoresList = JSON.parse(localStorage.getItem('highScoresStored')) || [];
+let userInitials;
+var userScoreObj;
+
+//set variable to track high scores being displayed
+let scoresDisplayed = false;
+
+//set current question to 1
+let currentQuestion = 1;
+
+//use event listeners for high score page buttons
+clearHighScoresButton.addEventListener('click', () => {
+    localStorage.clear();
+    leaderBoard.classList.addEventListener('hidden');
+});
+
+returnToStartButton.addEventListener('click', () => {
+    window.location.reload();
+});
+
+//create function to get high scores
+function getHighScores() {
+    //first stop the timer
+    clearInterval(beginCountdown);
+
+    //get the top 10 scores in the array
+    for (i=0; i < highScoresList.length; i++) {
+        var savedInitials = highScoresList[i].userInitials;
+        var savedScore = highScoresList[i].time;
+
+        var newScore = document.createElement('li');
+        newScore.textContent = `${i + 1}. ${savedInitials} -- ${savedScore}`;
+        leaderBoard.append(newScore);
+    }
+
+    //make high scores input page hidden and show display only
+    quizIntroContainer.classList.add('hidden');
+    endButtonElement.classList.add('hidden');
+    highScoresPage.classList.add('hidden');
+    highScoresElement.classList.add('hidden');
+    endGameText.classList.add('hidden');
+    answerContainer.classList.add('hidden');
+    formElement.classList.add('hidden');
+
+    //prevent same scores showing mulitple times
+    viewHighScores.getElementsByClassName.pointerEvents = "none";
+
+};
+
+//save high score function
+function saveHighScores() {
+    localStorage.setItem('highScoresStored', JSON.stringify(highScoresList));
+};
+
+//function to fetch user initials and score
+function getUserInfo() {
+    event.preventDefault();
+    userInitials = userInitialsElement.value;
+
+    if(!userInitials) {
+        alert(" Invalid answer, please enter your initials! ");
+        return;
+    }
+    userScoreObj = {
+        "initials": userInitials,
+        "time": counter,
+    };
+    //get high scores from localStorage
+    highScoreList = JSON.parse(localStorage.getItem('highScoresStored')) || [];
+
+    if (!highScoresList){
+        highScoresList.push(userScoreobj);
+        saveHighScores();
+    } else {
+        highScoresList.push(userScoreObj);
+        highScoresList.highScoreList((first,second) => second.time - first.time);
+        saveHighScores();
+    }
+    getHighScores(userScoreObj);
+};
+
+
+
